f01a9a9977a6991a0b9a46299c01be5dd2a78ba9
cafe small house <tbaram10@naver.com>
2023-03-28 17:36:26 +0900

    gitignore 추가

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Test = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <View>
+      <Text>그래도 괜찮은거 같은데?</Text>
       <Text>test</Text>
     </View>
   );
